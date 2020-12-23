d246a0ec3d74f2f7c2364afae9e831a20d2bbf34
Sander Dijkhuis <sander.dijkhuis@cleverbase.com>
2020-12-23 15:31:59 +0100

    style: reformat main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::App;
 
-mod document;
 mod authoring;
+mod document;
 mod publishing;
 
 #[tokio::main]
@@ -9,20 +9,14 @@ async fn main() {
     let matches = App::new("HashtagWiki")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Sander Dijkhuis <mail@sanderdijkhuis.nl>")
-        .subcommand(
-            App::new("author")
-                .about("serves dynamically locally")
-        )
-        .subcommand(
-            App::new("publish")
-                .about("writes static output to out dir")
-        )
+        .subcommand(App::new("author").about("serves dynamically locally"))
+        .subcommand(App::new("publish").about("writes static output to out dir"))
         .get_matches();
 
     match matches.subcommand() {
         None => println!("Use the help subcommand for help"),
         Some(("author", _)) => authoring::serve(([127, 0, 0, 1], 3030)).await,
         Some(("publish", _)) => publishing::run(),
-        Some((&_, _)) => println!("Unknown input")
+        Some((&_, _)) => println!("Unknown input"),
     }
 }
